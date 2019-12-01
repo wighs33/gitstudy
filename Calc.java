f3d1be7ce2e973f0cb115d72de5ca895a6336ace
wigwag <genji@kakao.com>
2019-12-01 18:40:25 +0900

    add a subtraction

--- Calc.java
+++ Calc.java
@@ -2,4 +2,7 @@ public class Calc {
 	public int add(int x, int y) {
 		return x+y;
 	}
+       public int sub(int x, int y) {
+		return x-y;
+	}
 }
--- CalcTest.java
+++ CalcTest.java
@@ -1,11 +1,12 @@
-import static org.junit.Assert.*;
-import org.junit.Test;
-
 public class CalcTest {
-
 	@Test
 	public void testAdd() {
 		Calc c = new Calc();
 		assertEquals(30, c.add(10, 20));
+	}
+         @Test
+	public void testSub() {
+		Calc c = new Calc();
+		assertEquals(-10, c.sub(10, 20));
 	}
 }
